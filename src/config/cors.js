6f10130a4270d733286dd344af72cfaa3fa0bb0e
Valber Siqueira <valberdevelop@gmail.com>
2018-04-19 16:05:42 -0300

    atualizando o cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,4 +1,4 @@
-module.exports = function(req, rep , next) {
+module.exports = function(req, res , next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH' )
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' )
